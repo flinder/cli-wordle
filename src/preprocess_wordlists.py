e8a9e0d9544937c2f9be923d5d0d27ec7c846958
Fridolin Linder <flinder@Fridolins-MBP.fritz.box>
2022-02-19 21:14:54 +0100

    txt word lists

--- src/preprocess_wordlists.py
+++ src/preprocess_wordlists.py
@@ -1,11 +1,10 @@
 import os
-import pickle
 import re
 import requests
 from wordle import Wordle
 
 WORD_LIST_URLS = {
-    'en_US': 'https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt',
+    'en_US': 'https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt',
     'de_DE': 'https://raw.githubusercontent.com/davidak/wortliste/master/wortliste.txt'
 }
 
@@ -14,24 +13,24 @@ def de_special_chars(word):
     special_chars = {'ä': 'ae', 'ö': 'oe', 'ü': 'ue', 'ß': 'ss'}
     for char, replacement in special_chars.items():
         word = re.sub(char, replacement, word)
+    return word
 
 
-def preprocess_word_list(word_list_url: str, output_file: str, word_lengths: list[int],
-                         language: str) -> None:
-
-    lens = set(word_lengths)
-    out = {length: [] for length in word_lengths}
+def preprocess_word_list(word_list_url: str, output_file: str, language: str) -> None:
 
     word_list = requests.get(word_list_url).content.decode('utf-8').split('\n')
+    out = set()
     for line in word_list:
         line = line.strip('\r\n')
-        if (word_length := len(line)) in lens and line.isalpha():
-            word = de_special_chars(line.lower()) if language == 'de_DE' else line.lower()
-            out[word_length].append(word)
+        word = de_special_chars(line.lower()) if language == 'de_DE' else line.lower()
+        if word and len(word) == Wordle.WORD_LENGTH and word.isalpha():
+            out.add(word)
 
-    print([f'{x}: {len(out[x])}' for x in out])
-    with open(output_file, 'wb') as outfile:
-        pickle.dump(out, outfile)
+    print(f'Parsed {len(out)} words for {language}')
+    with open(output_file, 'w') as outfile:
+        out = sorted(list(out))
+        for word in out:
+            outfile.write(word + '\n')
 
 
 if __name__ == '__main__':
@@ -39,6 +38,5 @@ if __name__ == '__main__':
     for lang, url in WORD_LIST_URLS.items():
         print(f'Processing {lang}')
         preprocess_word_list(word_list_url=url,
-                             output_file=os.path.join('../data/', lang + '.pkl'),
-                             word_lengths=Wordle.SUPPORTED_LENGTHS,
+                             output_file=os.path.join('../data/', lang + '.txt'),
                              language=lang)
